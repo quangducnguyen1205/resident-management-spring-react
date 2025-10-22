43ef625979bbcca79984916cfe9a2acd030861b2
ChinhDucLe <155630413+9hdzprovip@users.noreply.github.com>
2025-10-22 21:40:05 +0700

    repository taikhoan, hokhau

---
+++ src/main/java/com/example/QuanLyDanCu/repository/HoKhauRepository.java
@@ -0,0 +1,6 @@
+package com.example.QuanLyDanCu.repository;
+
+import com.example.QuanLyDanCu.entity.HoKhau;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HoKhauRepository extends JpaRepository<HoKhau, Long> { }
